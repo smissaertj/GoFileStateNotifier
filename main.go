131b771ae0ef0e1eddc89142bffbbdf4dc2a743a
Joeri Smissaert <smissaertj@gmail.com>
2024-08-11 10:05:27 +0300

    Refactor: Apply functional decomposition to improve code structure and readability

--- main.go
+++ main.go
@@ -9,20 +9,27 @@ import (
 
 func main() {
 	basePath := "/tmp/my/base_path"
+	outputFiles := []string{
+		"my_file.json",
+		"my_file_2.json",
+	}
+
+	errs := getFileInfo(basePath, outputFiles)
+	printErrors(errs)
+}
+
+func getFileInfo(basePath string, files []string) []error {
+	// Slice to hold all errors
 	errs := []error{}
 
+	// Check if the basePath exists
 	_, err := os.Stat(basePath)
 	if err != nil {
 		errs = append(errs, err)
 	}
 
-	outputFiles := [2]string{
-		"my_file.json",
-		"my_file_2.json",
-	}
-
 	// Iterate over outputFiles and get the fileInfo
-	for _, v := range outputFiles {
+	for _, v := range files {
 		filePath := path.Join(basePath, v)
 		fileInfo, err := os.Stat(filePath)
 		if err != nil {
@@ -30,17 +37,20 @@ func main() {
 		} else {
 			// File exists, check if the file size is greather than 0 bytes.
 			fileSize := fileInfo.Size()
-			if fileSize == 0 {
+			switch fileSize {
+			case 0:
 				err := errors.New("file has 0 bytes of data")
 				err = fmt.Errorf("%w: %s", err, filePath)
 				errs = append(errs, err)
-			} else {
+			default:
 				fmt.Printf("%s exists! File size: %d bytes\n", filePath, fileSize)
 			}
 		}
 	}
+	return errs
+}
 
-	// Print out the errors.
+func printErrors(errs []error) {
 	if len(errs) > 0 {
 		for _, e := range errs {
 			fmt.Println(e)
