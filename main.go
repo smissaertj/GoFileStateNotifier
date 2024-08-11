f8eb7a49ce410b2573866e32488dff0cb21ba6a4
Joeri Smissaert <smissaertj@gmail.com>
2024-08-11 16:49:32 +0300

    Refactor: Code layout, return early on error Feature: alertToSlack()

--- main.go
+++ main.go
@@ -1,34 +1,52 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path"
 )
 
-func main() {
-	basePath := "/tmp/my/base_path"
-	outputFiles := []string{
-		"my_file.json",
-		"my_file_2.json",
+type SlackBody struct {
+	Text string `json:"text"`
+}
+
+func alertToSlack(errs string, slackWebhook string) error {
+
+	jsonBody, err := json.Marshal(SlackBody{Text: errs})
+	if err != nil {
+		return fmt.Errorf("could not JSON encode data: %w", err)
+	}
+	fmt.Println("alertToSlack JSON Debug: ", string(jsonBody))
+
+	resp, err := http.Post(slackWebhook, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("slack API Post request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %w", err)
 	}
 
-	errs := getFileInfo(basePath, outputFiles)
-	slackErrorMsg := formatErrors(errs)
-	if slackErrorMsg != "" {
-		fmt.Println(slackErrorMsg)
-	}
+	fmt.Println("Debug Slack API Response: ", string(body))
+	return nil
 }
 
 func getFileInfo(basePath string, files []string) []error {
 	// Slice to hold all errors
 	errs := []error{}
 
-	// Check if the basePath exists
+	// Check if the basePath exists and exit early on error
 	_, err := os.Stat(basePath)
 	if err != nil {
 		errs = append(errs, err)
+		return errs
 	}
 
 	// Iterate over outputFiles and get the fileInfo
@@ -62,4 +80,27 @@ func formatErrors(errs []error) string {
 		}
 	}
 	return str
+}
+
+func main() {
+	slackWebhook := os.Getenv("SLACK_WEBHOOK")
+	if slackWebhook == "" {
+		fmt.Println("slack webhook environment variable missing")
+		os.Exit(1)
+	}
+
+	basePath := "/tmp/my/base_path"
+	outputFiles := []string{
+		"my_file.json",
+		"my_file_2.json",
+	}
+
+	errs := getFileInfo(basePath, outputFiles)
+	slackErrorMsg := formatErrors(errs)
+	if slackErrorMsg != "" {
+		fmt.Println(slackErrorMsg)
+		if err := alertToSlack(slackErrorMsg, slackWebhook); err != nil {
+			fmt.Println(err)
+		}
+	}
 }
